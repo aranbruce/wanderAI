2c2c7a88a5e1c3ccd14f7bfc3c425814f18b18b1
Aran Bruce-Caddick <aranbruce@gmail.com>
2023-07-21 09:03:50 +0100

    added white background to nav on scroll

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -1,19 +1,37 @@
-import React from 'react'
+'use client'
 
+import React, { useState, useEffect } from 'react'
 import Button from "../button/button"
 import Image from 'next/image'
-
 import styles from "./navBar.module.css"
 
 const NavBar = ({hideButton}) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${scrolled ? styles.navScrolled : ''}`}>
       <Image
-              src="./logo.svg"
-              alt="WanderAI Logo"
-              width={200}
-              height={48}
-              priority
+        src="./logo.svg"
+        alt="WanderAI Logo"
+        width={200}
+        height={48}
+        priority
       />
       {hideButton ? "" : <Button label="Sign up"/>}
     </nav>
