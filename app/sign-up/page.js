b1f068ef5587a9a9f27f313e8662018d90f1049b
Aran Bruce-Caddick <aranbruce@gmail.com>
2023-07-29 23:50:58 +0100

    fixed button issue and updated form submission

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -1,6 +1,9 @@
 'use client'
 
 import React, { useState }  from 'react'
+import { useForm } from '@formcarry/react';
+import {useRouter} from 'next/navigation';
+
 import va from '@vercel/analytics';
 import Section from "../../components/section/section";
 import Input from "../../components/input/input";
@@ -11,8 +14,16 @@ import styles from './page.module.css'
 const SignUp = () => {
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
-  console.log("fullname:", fullname);
-  console.log("email:", email);
+  const {state, submit} = useForm({
+    id: 'w4Ieb354Sc'
+  });
+
+  const router = useRouter();
+
+  // Success message
+  if (state.submitted) {
+    router.push("/thank-you");
+  }
 
   const handleFullNameChange = (e) => {
     setFullname(e.target.value);
@@ -29,30 +40,32 @@ const SignUp = () => {
           <h1 className={styles.title}>Create an account and join our waitlist</h1>
           <p className={styles.description}>Sign up now to get access to your full itinerary and plan your next great adventure in seconds</p>
         </div>
+
+
+
+
         {/* <form id="signUpForm" className={styles.form} onSubmit={handleSubmit}> */}
-        <form id="signUpForm" className={styles.form} action="https://formsubmit.co/34c673adc32695cc897b037f1e8e02e6" method="POST">
+        <form id="signUpForm" className={styles.form} onSubmit={submit}>
           <div className={styles.inputGroup}>
-          <Input
-            type="text"
-            inputMode="text"
-            value={fullname}
-            onChange={handleFullNameChange}
-            placeholder="Enter your full name"
-            label="Full name"
-            required
-          />
-          <Input
-            type="email"
-            inputmode="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder="Enter your email"
-            label="Email"
-            required
-          />
-          <input type="hidden" name="_captcha" value="false"/>
-          <input type="hidden" name="_next" value="https://wanderai.co.uk/thank-you"/>
-          </div>
+            <Input
+              type="text"
+              inputMode="text"
+              value={fullname}
+              onChange={handleFullNameChange}
+              placeholder="Enter your full name"
+              label="Full name"
+              required
+            />
+            <Input
+              type="email"
+              inputmode="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter your email"
+              label="Email"
+              required
+            />
+            </div>
           <Button label="Sign Up" type="submit" onClick={() => {va.track('Signup')}}/>
         </form>
       </div>
--- components/button/button.js
+++ components/button/button.js
@@ -7,7 +7,7 @@ import styles from "./button.module.css"
 const Button = ({type, variant, imageSrc, label, onClick, href}) => {
   return (
     href ? 
-    <Link href={href} onClick={onClick} style={{width: "fill-available"}} type={type} className={variant==="secondary" ? styles.buttonSecondary : styles.button}>
+    <Link href={href} onClick={onClick} type={type} className={variant==="secondary" ? styles.buttonSecondary : styles.button}>
       {imageSrc && <Image src={imageSrc} alt="Icon" width={24} height={24} />}
       {label && <span className={styles.text}>{label}</span>}
     </Link>
